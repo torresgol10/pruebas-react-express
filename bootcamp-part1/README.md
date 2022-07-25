91c209c37ee300c8d01eca24951d8c5a4a5aced2
torresgol10 <torresgol10.it@gmail.com>
2022-07-25 23:23:59 +0200

    Unificamos repositorios hijos.

---
+++ bootcamp-part1/README.md
@@ -0,0 +1,2 @@
+# bootcamp-part1
+Created with CodeSandbox
---
+++ bootcamp-part1/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Mensaje from "./Mensaje.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Mensaje color="red" message="Estamos trabajando" />
+    </div>
+  );
+}
